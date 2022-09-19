b87a4ae2c0cf6ece0eee0bae1779f9829db1ffcd
valopezs <valeriabrilopez@gmail.com>
2022-09-19 13:03:43 -0500

    created DB class

---
+++ Develop/db/DB.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const util = require("util");
+
+const noteData = "./db/db.json";
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// create DB class
+class DB {
+    async readNotes() {
+        try {
+            const notesRaw = await readFileAsync(noteData, "UTF8");
+            return notesRaw ? JSON.parse(notesRaw) : [];
+        }   catch (error) {
+            throw error;
+        }
+    }
+
+    async addNote(data) {
+        try {
+            await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
+                console.log("New note added!");
+            });
+        }   catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteNote(data) {
+        try {
+            await writeFileAsync (noteData, JSON.stringify(data, null, "\t")).then(() => {
+                console.log("Note Deleted!");
+            });
+        }   catch (error) {
+            throw error;
+        }
+    }
+}
+
+module.exports = new DB();
